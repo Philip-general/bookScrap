3ad33a80acff305369ae8b92e09708c3d99e52f3
MinGook <mgk8609@naver.com>
2022-01-10 10:52:31 +0900

    firebase 환경변수 설정

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import { initializeApp } from 'firebase/app'
+import { getDatabase } from 'firebase/database'
+import config from './confing'
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_DATABASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_URL,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  storageBucket: process.env.FIREBASE_STORAGE_URL,
+}
+
+const app = initializeApp(firebaseConfig)
+
+// Get a reference to the database service
+const database = getDatabase(app)
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{config.FIREBASE_DATABASE_API_KEY}</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +32,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
