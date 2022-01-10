ac58e8763a783a6c552037ff9d5459f8d5d1c765
MinGookK <mgk8609@naver.com>
2022-01-10 17:07:23 +0900

    stack navigator practice

--- App.js
+++ App.js
@@ -1,34 +1,55 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
-import { initializeApp } from 'firebase/app'
-import { getDatabase } from 'firebase/database'
-import config from './confing'
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import config from "./confing";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
 
 const firebaseConfig = {
   apiKey: config.FIREBASE_DATABASE_API_KEY,
   authDomain: config.FIREBASE_AUTH_URL,
   databaseURL: config.FIREBASE_DATABASE_URL,
   storageBucket: config.FIREBASE_STORAGE_URL,
-}
+};
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
 
-const app = initializeApp(firebaseConfig)
-// Get a reference to the database service
-const database = getDatabase(app)
-console.log(firebaseConfig)
-export default function App() {
+function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>asdf</Text>
-      <StatusBar style="auto" />
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Details")}
+      />
     </View>
-  )
+  );
+}
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
-})
+});
