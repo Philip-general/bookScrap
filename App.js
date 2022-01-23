33d42f3cfdb5db85e2c9d694b8c23a62f60f59bd
MinGook <mgk8609@naver.com>
2022-01-23 12:56:09 +0900

    home 책 검색 api 연동 / input값으로 저장되도록 해야 함

--- App.js
+++ App.js
@@ -11,9 +11,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Signup" component={Signup} />
-        <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   )
--- pages/Home.js
+++ pages/Home.js
@@ -1,12 +1,28 @@
-import { Text, View, Button, ActivityIndicator, TextInput } from "react-native";
-import { firebaseConfig } from "../firbase";
-import { styles } from "../components/common/style";
+import { Text, View, Button, ActivityIndicator, TextInput } from 'react-native'
+import { styles } from '../components/common/style'
+import axios from 'axios'
+import config from '../confing'
 
 export function Home({ navigation }) {
+  const handleSubmit = async bookName => {
+    try {
+      const res = await axios({
+        method: 'get',
+        // url: '/v3/search/book',
+        url: `https://dapi.kakao.com/v2/search/web?sort=accuracy&page=1&size=10&query=${bookName}`,
+        headers: {
+          Authorization: `KakaoAK ${config.KAKAO_BOOK_SEARCH_API_KEY}`,
+        },
+      })
+      console.log(res)
+    } catch (e) {
+      console.log(e)
+    }
+  }
   return (
     <View style={styles.container}>
-      <TextInput placeholder="책 제목을 입력해 주세요." style={styles.input} />
-      <Button title="책 추가하기" />
+      <TextInput placeholder="책 제목을 입력해 주to요." style={styles.input} />
+      <Button title="책 추가하기" onPress={handleSubmit} />
     </View>
-  );
+  )
 }
