901afce0081809ae381c461e13f4fe838a476c4b
MinGookK <mgk8609@naver.com>
2022-01-24 17:53:19 +0900

    책 제목 입력시 검색되기까지 완료

--- pages/Home.js
+++ pages/Home.js
@@ -1,28 +1,48 @@
-import { Text, View, Button, ActivityIndicator, TextInput } from 'react-native'
-import { styles } from '../components/common/style'
-import axios from 'axios'
-import config from '../confing'
+import { Text, View, Button, ActivityIndicator, TextInput } from "react-native";
+import { styles } from "../components/common/style";
+import axios from "axios";
+import config from "../confing";
+import { Controller, useForm } from "react-hook-form";
 
 export function Home({ navigation }) {
-  const handleSubmit = async bookName => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async ({ bookName }) => {
+    console.log(bookName);
     try {
       const res = await axios({
-        method: 'get',
+        method: "get",
         // url: '/v3/search/book',
-        url: `https://dapi.kakao.com/v2/search/web?sort=accuracy&page=1&size=10&query=${bookName}`,
+        url: `https://dapi.kakao.com/v3/search/book?sort=accuracy&page=1&size=10&query=${bookName}`,
         headers: {
           Authorization: `KakaoAK ${config.KAKAO_BOOK_SEARCH_API_KEY}`,
         },
-      })
-      console.log(res)
+      });
+      console.log(res.data.documents);
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
-  }
+  };
   return (
     <View style={styles.container}>
-      <TextInput placeholder="책 제목을 입력해 주to요." style={styles.input} />
-      <Button title="책 추가하기" onPress={handleSubmit} />
+      <Controller
+        control={control}
+        name="bookName"
+        render={({ field: { onChange, value } }) => (
+          <TextInput
+            placeholder="책 제목을 입력해 주세요."
+            style={styles.input}
+            onChangeText={(value) => onChange(value)}
+            value={value}
+            defaultValue=""
+          />
+        )}
+      />
+      <Button title="책 추가하기" onPress={handleSubmit(onSubmit)} />
     </View>
-  )
+  );
 }
