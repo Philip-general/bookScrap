1592bfc5e07915fd24fbc02b4cdc9a450d33068d
MinGookK <mgk8609@naver.com>
2022-02-08 13:16:09 +0900

    서버 파일 분리

--- App.tsx
+++ App.tsx
@@ -7,27 +7,27 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { store } from "./src/store/store";
 import { Provider } from "react-redux";
 import "./src/mocks/server";
-import { createServer } from "miragejs";
 import { Text, View } from "react-native";
 import React from "react";
 
-if (window.server) {
-  server.shutdown();
-}
+// const Stack = createNativeStackNavigator();
+// const queryClinet = new QueryClient();
 
-window.server = createServer({
-  routes() {
-    this.get("/api/movies", () => {
-      return {
-        movies: [
-          { id: 1, name: "Inception", year: 2010 },
-          { id: 2, name: "Interstellar", year: 2014 },
-          { id: 3, name: "Dunkirk", year: 2017 },
-        ],
-      };
-    });
-  },
-});
+// export default function App() {
+//   return (
+//     <Provider store={store}>
+//       <QueryClientProvider client={queryClinet}>
+//         <NavigationContainer>
+//           <Stack.Navigator>
+//             <Stack.Screen name="Home" component={Home} />
+//             {/* <Stack.Screen name="Login" component={Login} />
+//           <Stack.Screen name="Signup" component={Signup} /> */}
+//           </Stack.Navigator>
+//         </NavigationContainer>
+//       </QueryClientProvider>
+//     </Provider>
+//   );
+// }
 
 export default function App() {
   let [movies, setMovies] = React.useState([]);
@@ -48,21 +48,3 @@ export default function App() {
     </View>
   );
 }
-// const Stack = createNativeStackNavigator();
-// const queryClinet = new QueryClient();
-
-// export default function App() {
-//   return (
-//     <Provider store={store}>
-//       <QueryClientProvider client={queryClinet}>
-//         <NavigationContainer>
-//           <Stack.Navigator>
-//             <Stack.Screen name="Home" component={Home} />
-//             {/* <Stack.Screen name="Login" component={Login} />
-//           <Stack.Screen name="Signup" component={Signup} /> */}
-//           </Stack.Navigator>
-//         </NavigationContainer>
-//       </QueryClientProvider>
-//     </Provider>
-//   );
-// }
