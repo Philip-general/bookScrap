60a8c824f35ea97c90a351865e2eee91f9f48edd
joun008 <jouny007@gm.gist.ac.kr>
2022-02-13 23:45:14 +0900

    FEAT : 'Login창으로 연결 but, navigation다시'

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,7 +15,6 @@ export default function Home() {
     handleSubmit,
     formState: { errors },
   } = useForm();
-
   const searchBook = ({ bookName }) => {
     setBookName(bookName);
     refetch();
