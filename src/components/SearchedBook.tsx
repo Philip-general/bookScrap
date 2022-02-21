3a581752d7026c47579eb42dd6e134e3894f2d14
MinGookK <mgk8609@naver.com>
2022-02-21 23:05:19 +0900

    스크랩북 생성 하고 ok 뜨면 메인으로 이동하는 코드

--- src/components/SearchedBook.tsx
+++ src/components/SearchedBook.tsx
@@ -3,14 +3,23 @@ import React from "react";
 import { styles } from "../style";
 import { bookData } from '../../types/type';
 import { useAddScrapMutation } from '../hooks/addScrapBook';
+import { useNavigation } from '@react-navigation/native';
 
 
 export default function SearchedBook(Props: bookData) {
   const { mutate } = useAddScrapMutation();
-  const {title, authors, thumbnail} = Props
+  const {title, authors, thumbnail} = Props;
+  const navigation = useNavigation();
   const addScrap = () => {
     console.log('scrap 추가',title,authors)
-    mutate(Props)
+    mutate(Props,{
+      onSuccess: (data) => {
+        if(data.ok){
+          //이거 스크랩북 모여있는 메인페이지로 이동할 것. 지금 명명이 잘못됐음 ㅜ
+          navigation.navigate('RealMain')
+        }
+      }
+    })
   }
 
   return (
