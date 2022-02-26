511639d0d69b91ab6cb74e1bf3ee441ab04ce62a
MinGook <mgk8609@naver.com>
2022-02-26 17:47:12 +0900

    그냥 다 서버코드 주석처리함

--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -1,85 +1,95 @@
+// import {
+//   BASE_URL,
+//   LOGIN,
+//   MAKE_SCRAPBOOK,
+//   SEARCH_BOOK,
+//   SIGNUP,
+//   SCRAPBOOKS,
+// } from '../hooks/urls/url'
+// import { createServer } from 'miragejs'
 
-import { BASE_URL, LOGIN, MAKE_SCRAPBOOK, SEARCH_BOOK, SIGNUP, SCRAPBOOKS } from "../hooks/urls/url";
-import { createServer } from "miragejs";
+// if (window.server) {
+//   server.shutdown()
+// }
 
-if (window.server) {
-  server.shutdown();
-}
+// // "Mirage: Your app tried to POST 'http://3.37.129.186/apis/v1/login', but there was no route defined to handle this request. Define a route for this endpoint in your routes() config. Did you forget to define a namespace?"
+// window.server = createServer({
+//   routes() {
+//     // this.passthrough()
+//     this.namespace = BASE_URL
+//     this.get(`${SEARCH_BOOK}`, () => {
+//       return {
+//         documents: [
+//           {
+//             title: '더미책1',
+//             authors: ['김민국'],
+//             thumbnail:
+//               'https://search1.kakaocdn.net/thumb/R120x174.q85/?fname=http%3A%2F%2Ft1.daumcdn.net%2Flbook%2Fimage%2F1467038',
+//           },
+//           {
+//             title: '더미책2',
+//             authors: ['이건호', '조용환'],
+//             thumbnail:
+//               'https://search1.kakaocdn.net/thumb/R120x174.q85/?fname=http%3A%2F%2Ft1.daumcdn.net%2Flbook%2Fimage%2F1467038',
+//           },
+//         ],
+//       }
+//     })
 
-window.server = createServer({
-  routes() {
-    this.namespace = BASE_URL;
-    this.get(`${SEARCH_BOOK}`, () => {
-      return {
-        documents: [
-          {
-            title: "더미책1",
-            authors: ["김민국"],
-            thumbnail:
-              "https://search1.kakaocdn.net/thumb/R120x174.q85/?fname=http%3A%2F%2Ft1.daumcdn.net%2Flbook%2Fimage%2F1467038",
-          },
-          {
-            title: "더미책2",
-            authors: ["이건호", "조용환"],
-            thumbnail:
-              "https://search1.kakaocdn.net/thumb/R120x174.q85/?fname=http%3A%2F%2Ft1.daumcdn.net%2Flbook%2Fimage%2F1467038",
-          },
-        ],
-      };
-    });
-
-    // post
-    this.post(`${MAKE_SCRAPBOOK}`, (schema, request) => {
-      console.log(request)
-      return { ok: true }
-    });
-    this.post(`${LOGIN}`, (schema, request) => {
-      const { email, password } = JSON.parse(request.requestBody);
-      if (email === "123@gmail.com" && password === "123") {
-        return {
-          ok: true,
-          AccessToken: "1234",
-        };
-      } else {
-        return {
-          ok: false,
-          error: "계정 정보가 일치하지 않습니다.",
-        };
-      }
-    });
-    this.post(`${SIGNUP}`, (schema, request) => {
-      const { id, password, email } = JSON.parse(request.requestBody);
-      if (id === "조용환") {
-        return {
-          ok: false,
-        };
-      } else {
-        return {
-          ok: true,
-        };
-      }
-    });
-    this.get(`${SCRAPBOOKS}`, () => {
-      return{
-        documents:[
-          {
-            title: "더미책1",
-            authors: ["김민국"],
-            contents:"안녕하세요 더미책1입니다.가나다라마바사아자차카타파하ㅎㅎㅎㅎㅎㅎ",
-            thumbnail:
-              "https://search1.kakaocdn.net/thumb/R120x174.q85/?fname=http%3A%2F%2Ft1.daumcdn.net%2Flbook%2Fimage%2F1467038",
-            scrapbookId :1,
-          },
-          {
-            title: "더미책2",
-            authors: ["이건호", "조용환"],
-            contents:"안녕하세요 더미책2입니다.가나다라마바사아자차카타파하ㅎㅎㅎㅎㅎㅎ",
-            thumbnail:
-              "https://search1.kakaocdn.net/thumb/R120x174.q85/?fname=http%3A%2F%2Ft1.daumcdn.net%2Flbook%2Fimage%2F1467038",
-            scrapbookId :2,
-          },
-        ]
-      }
-    })
-  }
-});
+//     // post
+//     this.post(`${MAKE_SCRAPBOOK}`, (schema, request) => {
+//       console.log(request)
+//       return { ok: true }
+//     })
+//     this.post(`/login`, (schema, request) => {
+//       const { email, password } = JSON.parse(request.requestBody)
+//       if (email === '123@gmail.com' && password === '123') {
+//         return {
+//           ok: true,
+//           AccessToken: '1234',
+//         }
+//       } else {
+//         return {
+//           ok: false,
+//           error: '계정 정보가 일치하지 않습니다.',
+//         }
+//       }
+//     })
+//     this.post(`${SIGNUP}`, (schema, request) => {
+//       const { id, password, email } = JSON.parse(request.requestBody)
+//       if (id === '조용환') {
+//         return {
+//           ok: false,
+//         }
+//       } else {
+//         return {
+//           ok: true,
+//         }
+//       }
+//     })
+//     this.get(`${SCRAPBOOKS}`, () => {
+//       return {
+//         documents: [
+//           {
+//             title: '더미책1',
+//             authors: ['김민국'],
+//             contents:
+//               '안녕하세요 더미책1입니다.가나다라마바사아자차카타파하ㅎㅎㅎㅎㅎㅎ',
+//             thumbnail:
+//               'https://search1.kakaocdn.net/thumb/R120x174.q85/?fname=http%3A%2F%2Ft1.daumcdn.net%2Flbook%2Fimage%2F1467038',
+//             scrapbookId: 1,
+//           },
+//           {
+//             title: '더미책2',
+//             authors: ['이건호', '조용환'],
+//             contents:
+//               '안녕하세요 더미책2입니다.가나다라마바사아자차카타파하ㅎㅎㅎㅎㅎㅎ',
+//             thumbnail:
+//               'https://search1.kakaocdn.net/thumb/R120x174.q85/?fname=http%3A%2F%2Ft1.daumcdn.net%2Flbook%2Fimage%2F1467038',
+//             scrapbookId: 2,
+//           },
+//         ],
+//       }
+//     })
+//   },
+// })
