3df71d7549be6c4d5632f08723bbd347a31b93f8
MinGook <mgk8609@naver.com>
2022-03-09 12:30:43 +0900

    Auth 추가

---
+++ src/components/auth/Auth.tsx
@@ -0,0 +1,24 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import { useGetMe } from '../../hooks/user';
+import { useNavigation } from '@react-navigation/core';
+
+export default function Auth({ children }) {
+  const { data: userData, isLoading } = useGetMe();
+  const navigation = useNavigation();
+
+  if (isLoading) {
+    return null;
+  } else {
+    if (userData?.id) {
+      return (
+        <View>
+          <Text>Auth 컴포넌트</Text>
+          {children}
+        </View>
+      );
+    } else {
+      navigation.replace('Login');
+    }
+  }
+}
