7249648c272a91db52b03af553b56aa2a5130475
MinGook <mgk8609@naver.com>
2022-03-15 14:01:54 +0900

    .

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,64 +1,97 @@
-import { useNavigation } from "@react-navigation/native";
-import React from "react";
-import { View } from "react-native";
-import { styles } from "../style";
-import {useGetScrapBooks} from "../hooks/scrapbook"
-import { ScrapbookData } from "../../types/type";
-import Myscrapbook from "../components/Myscrapbook";
-import { useGetMe } from "../hooks/user";
-import Loading from "../components/Loading";
+import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+import { View } from 'react-native';
+import { styles } from '../style';
+import { useGetScrapBooks } from '../hooks/scrapbook';
+import { ScrapbookData } from '../../types/type';
+import Myscrapbook from '../components/Myscrapbook';
+import { useGetMe } from '../hooks/user';
+import Loading from '../components/Loading';
 
-export default function Main(){
-    const navigation = useNavigation();
-    const {data:userData,isSuccess } = useGetMe();
-    if(isSuccess){
-        if(userData.user==null ||userData.user==undefined){
-            navigation.replace("Login")
-        }
+export default function Main() {
+  const navigation = useNavigation();
+  const { data: userData, isSuccess } = useGetMe();
+  if (isSuccess) {
+    console.log('asdfasdf', userData);
+    if (userData.user == null || userData.user == undefined) {
+      console.log('fffff');
+      navigation.replace('Login');
     }
-    
-    const {data:scrapBookData, isLoading:scrapLoding, isError, refetch } = useGetScrapBooks();
-    return(
-        <View style={styles.Main}>
-            <View style={styles.Main_medium}>
-                {scrapLoding? (<Loading/>):
-                (<View>
-                    {scrapBookData.documents?.map(({ title, authors, thumbnail,contents,scrapbookId,countscrap,useGroup,fixpoint }: ScrapbookData ) => {
-                        if(fixpoint==true){
-                           return (
-                            <Myscrapbook
-                            key={scrapbookId}
-                            authors={authors}
-                            thumbnail={thumbnail}
-                            title={title}
-                            contents={contents}
-                            scrapbookId={scrapbookId}
-                            countscrap={countscrap}
-                            useGroup={useGroup}
-                            fixpoint={fixpoint}
-                            />
-                            );
-                        }
-                    })}
-                    {scrapBookData.documents?.map(({ title, authors, thumbnail,contents,scrapbookId,countscrap,useGroup,fixpoint }: ScrapbookData ) => {
-                        if(fixpoint==false){
-                           return (
-                            <Myscrapbook
-                            key={scrapbookId}
-                            authors={authors}
-                            thumbnail={thumbnail}
-                            title={title}
-                            contents={contents}
-                            scrapbookId={scrapbookId}
-                            countscrap={countscrap}
-                            useGroup={useGroup}
-                            fixpoint={fixpoint}
-                            />
-                            );
-                        }
-                    })}
-                </View>)}
-            </View>
-        </View>     
-    )
-}   
+  }
+
+  const {
+    data: scrapBookData,
+    isLoading: scrapLoding,
+    isError,
+    refetch,
+  } = useGetScrapBooks();
+  console.log('sdf', scrapBookData);
+  return (
+    <View style={styles.Main}>
+      <View style={styles.Main_medium}>
+        {scrapLoding ? (
+          <Loading />
+        ) : (
+          <View>
+            {scrapBookData.documents?.map(
+              ({
+                title,
+                authors,
+                thumbnail,
+                contents,
+                scrapbookId,
+                countscrap,
+                useGroup,
+                fixpoint,
+              }: ScrapbookData) => {
+                if (fixpoint == true) {
+                  return (
+                    <Myscrapbook
+                      key={scrapbookId}
+                      authors={authors}
+                      thumbnail={thumbnail}
+                      title={title}
+                      contents={contents}
+                      scrapbookId={scrapbookId}
+                      countscrap={countscrap}
+                      useGroup={useGroup}
+                      fixpoint={fixpoint}
+                    />
+                  );
+                }
+              }
+            )}
+            {scrapBookData.documents?.map(
+              ({
+                title,
+                authors,
+                thumbnail,
+                contents,
+                scrapbookId,
+                countscrap,
+                useGroup,
+                fixpoint,
+              }: ScrapbookData) => {
+                if (fixpoint == false) {
+                  return (
+                    <Myscrapbook
+                      key={scrapbookId}
+                      authors={authors}
+                      thumbnail={thumbnail}
+                      title={title}
+                      contents={contents}
+                      scrapbookId={scrapbookId}
+                      countscrap={countscrap}
+                      useGroup={useGroup}
+                      fixpoint={fixpoint}
+                    />
+                  );
+                }
+              }
+            )}
+          </View>
+        )}
+      </View>
+    </View>
+  );
+}
